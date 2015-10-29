389fa17e805de8eee668b66adae2af615b007217
Murilo Borges <murilogborges@gmail.com>
2015-10-29 10:02:14 -0200

    Update inheritanceFiltering.sh

--- bin/inheritanceFiltering.sh
+++ bin/inheritanceFiltering.sh
@@ -589,7 +589,7 @@ function filterInheritance
 vcf=`echo $@ | awk -F " " '{print $1}'`
 inheritance=`echo $@ | awk -F " " '{print $2}'`
 phenotype=`echo $@ | awk -F " " '{print $3}'`
-individuals="`echo $@ | awk -F " " '{print $4}'`"
+individuals=`echo $@ | awk -F " " '{print $4}'`
 relation=`echo $@ | awk -F " " '{print $5}'`
 
 isThereHelp=`echo $@ | grep '\-help' -c`
@@ -611,7 +611,8 @@ fi
 splitVCF $vcf
 
 vcfs=""
-splitSamples=`echo $individuals | sed 's/,/ /g'`
+s=$individuals
+splitSamples=`echo $s | sed 's/,/ /g'`
 
 for i in $splitSamples
 do
@@ -624,6 +625,7 @@ do
 getGenotypes $v > $v.phenotype
 splitPhenotypes $v.phenotype
 done
+
 $inheritance $phenotype $individuals $relation > /tmp/$vcf.pos.tmp
 getVCF $vcf /tmp/$vcf.pos.tmp
 }
